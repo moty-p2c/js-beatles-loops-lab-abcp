fcad7e021e420fe36c1feb6c9f05f258f8c77158
moty pitusy <moty.p2c@gmail.com>
2018-01-20 10:51:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function theBeatkesPlay(musicians,instruments){
+function theBeatlesPlay(musicians,instruments){
   let newArr = [];
   for (var i=0 ;i<musicians.length; i++){
     newArr.push(musicians[i]+' plays '+instruments[i]);
